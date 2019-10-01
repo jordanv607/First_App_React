cbae812189bed5d5308419dee6a998a93128e9b9
Jordán Vargas <jordanv607@gmail.com>
2019-10-01 11:57:44 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 //permite devolver que es lo queremos 'pintar en la pantalla'
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      This is my component: 
+      <Helloworld text="texto1" /> 
+      <Helloworld text="texto2" /> 
+      <Helloworld text="texto3" /> 
+      </div>
+  );
+}
+
+//Componente
+function Helloworld(props){
+  return (
+    <div>{props.text}</div>
   );
 }
 
