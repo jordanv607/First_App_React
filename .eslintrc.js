40bcf2ed76b4a4957a6732e181b83aa5c1045c31
jordanv607 <jordanv607@gmail.com>
2020-04-14 14:10:23 -0600

    fixes of dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,6 @@ module.exports = {
       sourceType: 'module',
     },
     rules: {
-      // You can do your customizations here...
-      // For example, if you don't want to use the prop-types package,
-      // you can turn off that recommended rule with: 'react/prop-types': ['off']
+      'react/prop-types': ['off']
     },
   };
